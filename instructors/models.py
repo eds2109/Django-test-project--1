c27321454e670494ad9bc435bdc07fceb82d8393
eds2109 <warlock609@gmail.com>
2021-09-30 13:32:52 +0300

    add ManyToManyField

--- instructors/models.py
+++ instructors/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Course(models.Model):
+    name = models.CharField(null=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Instructor(models.Model):
-    name = models.CharField(verbose_name=u'Instructor Name', max_length=255, help_text='This is name')
+    #name = models.CharField(verbose_name=u'Instructor Name', max_length=255, help_text='This is name')
     surname = models.CharField(max_length=255)
     date_of_birth = models.DateField(null=True, blank=True)
     email = models.EmailField(unique=True, null=True)
 
-    course = models.CharField(max_length=255, null=True)
+    courses = models.ManyToManyField(Course)
     is_active = models.BooleanField(default=True)
-
