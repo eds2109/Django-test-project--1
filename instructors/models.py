df066206a784ad3dd081542ce2d310440b170b99
eds2109 <warlock609@gmail.com>
2021-09-30 14:55:08 +0300

    fix add models

--- instructors/models.py
+++ instructors/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Course(models.Model):
-    name = models.CharField(null=True, max_length=255)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Instructor(models.Model):
-    #name = models.CharField(verbose_name=u'Instructor Name', max_length=255, help_text='This is name')
+
+    name = models.CharField(verbose_name=u'Instructor Name', max_length=255, help_text='This is name')
     surname = models.CharField(max_length=255)
     date_of_birth = models.DateField(null=True, blank=True)
     email = models.EmailField(unique=True, null=True)
 
     courses = models.ManyToManyField(Course)
     is_active = models.BooleanField(default=True)
+
+    position = models.ForeignKey(Position, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
