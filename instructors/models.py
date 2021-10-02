9bddeeb3c74adcddeaf771c0add0d6d7299286cb
eds2109 <warlock609@gmail.com>
2021-10-02 10:45:34 +0300

    add models courses

--- instructors/models.py
+++ instructors/models.py
@@ -19,7 +19,7 @@ class Instructor(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     email = models.EmailField(unique=True, null=True)
 
-    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+    courses = models.ManyToManyField(Course)
     is_active = models.BooleanField(default=True)
 
     position = models.ForeignKey(Position, on_delete=models.PROTECT)
