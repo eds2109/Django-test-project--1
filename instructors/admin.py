f579b757b536adcfabefbc4a398e7a56ee327fbf
eds2109 <warlock609@gmail.com>
2021-10-05 10:24:36 +0300

    add exclude field

--- instructors/admin.py
+++ instructors/admin.py
@@ -3,6 +3,8 @@ from instructors.models import Instructor, Position, Course
 
 class InstructorAdmin(admin.ModelAdmin):
     list_display = ['name', 'surname']
+    #fields = ['name', 'surname', 'email', 'position']
+    exclude = ['date_of_birth']
 
 admin.site.register(Instructor, InstructorAdmin)
 admin.site.register(Position)
